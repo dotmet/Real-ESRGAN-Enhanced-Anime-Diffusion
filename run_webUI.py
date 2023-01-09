23261d627030866f55edf2958736393eb71c0c71
Mension <72541408+dotmet@users.noreply.github.com>
2023-01-09 21:20:56 +0800

    Update run_webUI.py

--- run_webUI.py
+++ run_webUI.py
@@ -1,11 +1,14 @@
 import os
 
-try:
-    from RealESRGANv030.interface import realEsrgan
-except:
-    os.system('cd RealESRGANv030 && python setup.py develop')
-
-status = os.system('python app.py')
-if status==256:
-    print('Run failed, try set MKL_THREADING_LAYER=GNU\n')
-    os.system('export MKL_THREADING_LAYER=GNU && python app.py')
+def check_RealESRGAN():
+    try:
+        from RealESRGANv030.interface import realEsrgan
+    except:
+        os.system('cd RealESRGANv030 && python setup.py develop')
+        
+if name=='__main__':
+    check_RealESRGAN()
+    status = os.system('python app.py')
+    if status==256:
+        print('Run failed, try set MKL_THREADING_LAYER=GNU\n')
+        os.system('export MKL_THREADING_LAYER=GNU && python app.py')
