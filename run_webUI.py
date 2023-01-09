f439a881ebe4adb7bd9e5a239321d943b9d3981d
Gddr100x <77543771@qq.com>
2023-01-09 18:32:34 +0800

    Update run_webUI.py

--- run_webUI.py
+++ run_webUI.py
@@ -6,6 +6,6 @@ except:
     os.system('cd RealESRGANv030 && python setup.py develop')
 
 status = os.system('python app.py')
-if status!=0:
+if status==256:
     print('Run failed, try set MKL_THREADING_LAYER=GNU\n')
     os.system('export MKL_THREADING_LAYER=GNU && python app.py')
